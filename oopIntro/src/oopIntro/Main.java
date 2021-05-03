bbea6203ade75d84bbcaacca31cfeb67b7ae49aa
admin <admin@DESKTOP-O6P3COI>
2021-05-03 05:02:51 +0300

    deneme

--- oopIntro/src/oopIntro/Main.java
+++ oopIntro/src/oopIntro/Main.java
@@ -4,33 +4,21 @@ public class Main {
 
 	public static void main(String[] args) {
 		
-		Product product1 = new Product();
-		product1.id = 1;
-		product1.name = "Lenavo V14";
-		product1.unitPrice = 1500;
-		product1.detail = "16 GB Ram";
+		Product product1 = new Product(1,"Lenavo V14", 15000,"16 GB RAM",10);
 		
 		Product product2 = new Product();
-		product2.id = 2;
-		product2.name = "Lenavo V15";
-		product2.unitPrice = 1600;
-		product2.detail = "32 GB Ram";
+		product2.setId(2);
+		product2.setName("Lenavo V15");
+		product2.setDetail("16 gb Ram");
+		product2.setDiscount(10);
+		product2.setUnitPrice(1600);
 		
-		Product product3 = new Product();
-		product3.id = 3;
-		product3.name = "HP 5";
-		product3.unitPrice = 1600;
-		product3.detail = "32 GB Ram";
+		System.out.println(product2.getUnitPriceAfterDiscount());
 		
 		
-		Product[] products = {product1,product2,product3};
 		
-		for (Product product : products) {
-			
-			System.out.println(product.name);
-		}
 		
-		System.out.println(products.length);
+		
 	}
 
 } 
